e4e1064b1956215b3ab3de48dfa9add020a09f34
Caroline Berg <carolineeberg@gmail.com>
2018-07-22 14:13:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -70,10 +70,9 @@ def winner(board)
     position_3 = board[win_index_3]
     if position_1 == "X"  && position_2 == "X" && position_3 == "X"
       return "X"
-    elsif position_1 == "O"  && position_2 == "O" && position_3 == "O"
+    else position_1 == "O"  && position_2 == "O" && position_3 == "O"
       return "O"
-    else
-      return nil
     end
   end
+  return nil
 end
